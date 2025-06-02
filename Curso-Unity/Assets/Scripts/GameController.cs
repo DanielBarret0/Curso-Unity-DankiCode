aba0f5657ba72786d5718838edcdbe4e4cf81448
lalyandrade <josedanielbarreto@gmail.com>
2025-06-02 15:42:53 -0300

    save de moeda feito

--- Curso-Unity/Assets/Scripts/GameController.cs
+++ Curso-Unity/Assets/Scripts/GameController.cs
@@ -6,19 +6,19 @@ public class GameController : MonoBehaviour
     public Text healthText;
     public int score;
     public Text scoreText;
+    public int totalScore;
     public static GameController instance;
     // Chamando antes de tds os Start de tds ops scripts
     void Awake()
     {
         instance = this;
-
     }
 
     void Start()
     {
-        Debug.Log(PlayerPrefs.GetInt("score"));
+        totalScore = PlayerPrefs.GetInt("score");
+        Debug.Log("Total Score: " + totalScore);	
     }
-
 
     // Update is called once per frame
     void Update()
@@ -27,11 +27,12 @@ public class GameController : MonoBehaviour
     }
 
     public void UpdateScore(int value)
-    {
+    {   
+        //Soma e atribui o valor ao score/Moeda
         score += value;
         scoreText.text = score.ToString();
         // Variavel ficou com nome score SALVO
-        PlayerPrefs.SetInt("score", score);
+        PlayerPrefs.SetInt("score", score + totalScore);
     }
 
     public void UpdateHealthText(int value)
