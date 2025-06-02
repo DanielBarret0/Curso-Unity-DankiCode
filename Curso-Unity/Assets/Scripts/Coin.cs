061e57e81f01469690717d50fc8edfdf4300c2ff
lalyandrade <josedanielbarreto@gmail.com>
2025-06-02 15:24:39 -0300

    pontuação feita

---
+++ Curso-Unity/Assets/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public int scoreValue;
+    
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameController.instance.UpdateScore(scoreValue);
+            Destroy(gameObject);
+        }
+    }
+}
+
--- Curso-Unity/Assets/Scripts/GameController.cs
+++ Curso-Unity/Assets/Scripts/GameController.cs
@@ -4,17 +4,34 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public Text healthText;
+    public int score;
+    public Text scoreText;
     public static GameController instance;
     // Chamando antes de tds os Start de tds ops scripts
     void Awake()
     {
         instance = this;
+
     }
+
+    void Start()
+    {
+        Debug.Log(PlayerPrefs.GetInt("score"));
+    }
+
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void UpdateScore(int value)
+    {
+        score += value;
+        scoreText.text = score.ToString();
+        // Variavel ficou com nome score SALVO
+        PlayerPrefs.SetInt("score", score);
     }
 
     public void UpdateHealthText(int value)
